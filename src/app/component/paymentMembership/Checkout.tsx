0cb8bf5aa78948a3dfce6c45943b9050105d4487
WonhyeongLee <bigfile57@naver.com>
2024-01-29 10:47:07 +0900

    Feat: Checkout의 두 컴포넌트에 적용할 애니메이션 작성
    - SponsorsReview와 MembershipPrice에 div로 감싸서 ref를 사용해 적용했는데 Checkout의 ref를 전달하는 방식으로 수정 할 예정

--- src/app/component/paymentMembership/Checkout.tsx
+++ src/app/component/paymentMembership/Checkout.tsx
@@ -1,11 +1,46 @@
+import { useRef } from "react";
 import MembershipPrice from "../membershipPrice/MembershipPrice"
 import SponsorsReview from "../sponsorsReview/SponsorsReview"
 import * as styles from './Checkout.css'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const Checkout = () => {
+  const checkoutRef = useRef<HTMLDivElement>(null);
+  const sponsorsReviewRef = useRef<HTMLDivElement>(null);
+  const membershipPriceRef = useRef<HTMLDivElement>(null);
+
+  // Checkout section의 ref를 넘겨서 사용하도록 수정해야함
+  useGSAP(() => {
+    const checkoutTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: checkoutRef.current,
+        start: "top top",
+        end: "bottom top-=200",
+        scrub: 1,
+        pin: true,
+        toggleActions: "play reverse play reverse",
+      },
+    });
+
+    checkoutTl.from(sponsorsReviewRef.current, {
+      yPercent: -50,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.InOut",
+    });
+
+    checkoutTl.from(membershipPriceRef.current, {
+      yPercent: -20,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out"
+    });
+  });
+  
   return (
-    <section className={styles.CheckoutContainer}>
-      <SponsorsReview />
-      <MembershipPrice />
+    <section className={styles.CheckoutContainer} ref={checkoutRef}>
+        <SponsorsReview />
+        <MembershipPrice />
     </section>
   )
 }
