1a0488ebf7256905badd67a375b1eb5b15f2ab25
WonhyeongLee <bigfile57@naver.com>
2024-02-19 23:55:22 +0900

    Fix: mockServiceWorker 버전달랐던 문제 수정, handler의 get 요청 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,13 @@
 import { http, HttpResponse } from 'msw';
 
-import { membershipInformation } from '@/model/MembershipInformation.ts';
+import { membershipInformation } from '@/model/membershipInformation.ts';
 
 export const handlers = [
-  http.get('/api/membershipInformation', () => {
-    return HttpResponse.json(membershipInformation);
-  }),
+  http.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/membership-information`,
+    () => {
+      console.log('membership-information 요청을 처리합니다.');
+      return HttpResponse.json(membershipInformation);
+    },
+  ),
 ];
