33155b0277e3b0c592b5a06a7638e295c064d2c1
WonhyeongLee <bigfile57@naver.com>
2024-02-21 21:58:40 +0900

    Fix: MSW 버전 2.1로 다운, handler 수정

--- src/app/_component/main/Main.tsx
+++ src/app/_component/main/Main.tsx
@@ -1,25 +1,26 @@
-// import { getMembershipInfo } from '@/app/_lib/getMembershipInfo.ts';
 import Checkout from '../checkout/Checkout';
 import CommonBenefits from '../commonBenefits/CommonBenefits';
 import Introduction from '../introduction/Introduction';
 import MembershipBenefits from '../membershipBenefits/MembershipBenefits';
 
-async function getMembershipInfo() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/membershipInformation`,
-  );
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  const commonBenefitsData = await res.json();
+import type { MembershipInformation } from '@/model/membershipInformation.ts';
 
-  return commonBenefitsData;
+async function fetchCommonBenefits(): Promise<MembershipInformation> {
+  const response = await fetch(
+    // `${process.env.NEXT_PUBLIC_BASE_URL}/api/membership-information`,
+    `http://localhost:9090/api/membership-information`,
+  );
+  if (!response.ok) {
+    throw new Error('서버로부터 데이터를 가져오는 데 실패했습니다.');
+  }
+  const data = (await response.json()) as MembershipInformation;
+  return data;
 }
 
 const Main = async () => {
-  const data = await getMembershipInfo();
-
-  console.log(data);
+  const { commonBenefits, membershipBenefits, paymentOptions } =
+    await fetchCommonBenefits();
+  console.log(commonBenefits, membershipBenefits, paymentOptions);
 
   return (
     <>
@@ -32,4 +33,3 @@ const Main = async () => {
 };
 
 export default Main;
-// commonBenefits, membershipBenefits, paymentOptions
