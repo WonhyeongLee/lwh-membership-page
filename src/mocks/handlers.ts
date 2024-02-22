62af63573467be668ee95977f76e8c5d4dcb1cdc
WonhyeongLee <bigfile57@naver.com>
2024-02-22 22:44:09 +0900

    Fix: msw2.0 이상 버전에서 한글로 데이터를 반환할 때 json 파싱 오류가 뜨는 부분 수정
    - 계속 파싱 오류가 발생해서 로그를 찍어보니 데이터에 한글이 포함되었을 떄만 content-Length가 정상적으로 전달되지 않는거 같았습니다.
    - 따라서 handler에 전달할 json 데이터를 직렬화 해서 실제 바이트 크기를 구해 헤더에 포함시켜서 보내도록 추가해서 오류가 나지 않도록 수정했습니다.

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,21 @@
 import { http, HttpResponse } from 'msw';
 
 import { membershipInformation } from '@/model/membershipInformation.ts';
+
 /* eslint-disable prettier/prettier */
 /* eslint-disable quotes */
+const membershipInformationJson = JSON.stringify(membershipInformation);
+const byteSize = new TextEncoder().encode(membershipInformationJson).byteLength;
+
 export const handlers = [
   http.get('/api/membership-information', () => {
-    console.log('received request');
-    return HttpResponse.json(membershipInformation);
+    return HttpResponse.json(
+      membershipInformation,
+      {
+        headers: {
+          'Content-Length': String(byteSize),
+        },
+      },
+    );
   }),
 ];
