ef1ddfdffac7e462483d9b0a96b6dfd77996b568
WonhyeongLee <bigfile57@naver.com>
2024-02-22 22:40:44 +0900

    Chore: getMembershipInfo 단순수정

--- src/app/_lib/getMembershipInfo.ts
+++ src/app/_lib/getMembershipInfo.ts
@@ -1,16 +1,14 @@
 export async function getMembershipInfo() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/membershipInformation`,
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/membership-information`,
     {
       credentials: 'include',
       cache: 'no-store',
     },
   );
-  if (!res.ok) {
+  if (!response.ok) {
     throw new Error('Failed to fetch data');
   }
-  // 응답 본문을 한 번만 읽습니다.
-  // const data = await res.json();
 
-  return res.json();
+  return response.json();
 }
