b376ead9eaca650f7554652871f936b20a89c2a9
WonhyeongLee <bigfile57@naver.com>
2024-03-18 17:47:53 +0900

    Refactor: Main 컴포넌트를 삭제하고 page.tsx로 이동
    - page.tsx에서 Main 컴포넌트를 import해서 사용할 필요가 없어서 Main 컴포넌트를 제거하고 해당 내용을 page.tsx로 이동

--- src/app/MainPage.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const mainPageHeading = style({
-  color: 'red',
-  fontSize: '50px',
-  fontWeight: 'bold',
-  margin: '0',
-});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Main from '@/app/Main.tsx';
+import CommonBenefits from '@/app/_component/commonBenefits/CommonBenefits.tsx';
+import Introduction from '@/app/_component/introduction/Introduction.tsx';
+import MembershipBenefits from '@/app/_component/membershipBenefits/MembershipBenefits.tsx';
+import MembershipGateway from '@/app/_component/membershipGateway/MembershipGateway.tsx';
+import { getMembershipInfo } from '@/app/_lib/getMembershipInfo.ts';
 
-const MainPage = () => {
+const MainPage = async () => {
+  const {
+    commonBenefits,
+    membershipBenefits,
+    paymentOptions,
+    sponsorsReviews,
+  } = await getMembershipInfo();
+
   return (
     <>
-      <Main />
+      <Introduction />
+      <CommonBenefits commonBenefits={commonBenefits} />
+      <MembershipBenefits membershipBenefits={membershipBenefits} />
+      <MembershipGateway
+        sponsorsReviews={sponsorsReviews}
+        paymentOptions={paymentOptions}
+      />
     </>
   );
 };
