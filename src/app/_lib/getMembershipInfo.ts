8273a3a76419fc496cbe9baaa4aefcf64680edf6
WonhyeongLee <bigfile57@naver.com>
2024-03-18 21:01:06 +0900

    Feat: Mock api에 맞게 route handler에 요청 추가, getMembershipInfo에 조건 별 baseUrl 추가 #26

--- src/app/_lib/getMembershipInfo.ts
+++ src/app/_lib/getMembershipInfo.ts
@@ -1,11 +1,13 @@
+const baseUrl =
+  process.env.NEXT_PUBLIC_API_MOCK_ENABLED === 'enabled'
+    ? process.env.NEXT_PUBLIC_MOCK_URL // Mock 서버 URL
+    : process.env.NEXT_PROD_URL;
+
 export async function getMembershipInfo() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/membership-information`,
-    {
-      credentials: 'include',
-      cache: 'no-store',
-    },
-  );
+  const response = await fetch(`${baseUrl}/api/membership-information`, {
+    credentials: 'include',
+    cache: 'no-store',
+  });
   if (!response.ok) {
     throw new Error('Failed to fetch data');
   }
