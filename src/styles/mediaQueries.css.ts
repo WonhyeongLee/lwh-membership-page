9860065b49c6551a00534190f0f2dde304145d3a
WonhyeongLee <bigfile57@naver.com>
2024-03-26 16:45:40 +0900

    Style: 반응형 스타일 추가를 위해 반응형포인트 작성

---
+++ src/styles/mediaQueries.css.ts
@@ -0,0 +1,8 @@
+export const mediaQueries = {
+  mobileSmall: '(max-width: 320px)',
+  mobileMedium: '(min-width: 321px) and (max-width: 480px)',
+  mobileLarge: '(min-width: 481px) and (max-width: 768px)',
+  tablet: '(min-width: 769px) and (max-width: 1024px)',
+  laptop: '(min-width: 1025px) and (max-width: 1366px)',
+  desktopLarge: '(min-width: 1367px)',
+};
