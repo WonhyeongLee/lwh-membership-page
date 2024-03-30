c219457a64701ff5e4f838458c1c039c1ec355eb
WonhyeongLee <bigfile57@naver.com>
2024-03-30 16:23:17 +0900

    Style: GSAPWrapper 컴포넌트에 모바일 환경에서의 동적 vh를 위한 코드 추가, RootLayout에 적용

--- src/app/RootLayout.css.ts
+++ src/app/RootLayout.css.ts
@@ -4,11 +4,13 @@ import { mediaQueries } from '@/styles/mediaQueries.css.ts';
 import { baseTheme } from '@/styles/themes.css.ts';
 
 export const MainContainer = style({
+  height: 'calc(var(--vh, 1vh) * 100)',
   display: 'grid',
   gridTemplateColumns: '1fr',
   padding: '0',
   backgroundColor: baseTheme.color.background,
   '@media': {
+    [mediaQueries.mobile]: {},
     [mediaQueries.laptop]: {
       padding: '0 30px',
     },
