7fdc429e2921b5efa61a70301b9c3ee52200b602
WonhyeongLee <bigfile57@naver.com>
2024-04-10 16:08:59 +0900

    Feat: membershipInformation mock 데이터의 SponsorsReviews 부분에 id 속성 추가

--- src/model/membershipInformation.ts
+++ src/model/membershipInformation.ts
@@ -24,6 +24,7 @@ export interface PaymentOption {
   price: number;
 }
 export interface SponsorsReviews {
+  id: string;
   name: string;
   review: string;
   rating: number;
@@ -91,21 +92,25 @@ export const membershipInformation: MembershipInformation = {
   ],
   sponsorsReviews: [
     {
+      id: '1',
       name: '근박',
       review: '이길수있어요. 힘내봐요. 서렌치지마요.',
       rating: 5,
     },
     {
+      id: '2',
       name: '기박',
       review: '돈이 아깝고 머리가 아파요. 0점이 없어서 1점 드릴게요.',
       rating: 1,
     },
     {
+      id: '3',
       name: 'TEST',
       review: '돈이 아깝고 머리가 아파요. 0점이 없어서 1점 드릴게요.',
       rating: 1,
     },
     {
+      id: '4',
       name: '김현',
       review: '코스피 나스닥 빝코 다 떨어지고 있어요. 살려주세요',
       rating: 1,
